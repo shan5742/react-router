875bc6c52145996ae27f834c02ab7320e222c3c0
shan5742 <asamshan456@gmail.com>
2018-10-31 12:43:35 +0000

    adding nav component

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  <nav className="nav-wrapper red darken-3">
+    <div className="container">
+      <a className="brand-logo">Mario World</a>
+    </div>
+  </nav>;
+};
