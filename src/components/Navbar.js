ac2c9d1f6e66fb8d0ae78b73e771d201eb5edde0
shan5742 <asamshan456@gmail.com>
2018-10-31 12:46:30 +0000

    adding nav links and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,19 @@ const Navbar = () => {
   <nav className="nav-wrapper red darken-3">
     <div className="container">
       <a className="brand-logo">Mario World</a>
+      <ul className="right">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/About">About</a>
+        </li>
+        <li>
+          <a href="/Contact">Contact</a>
+        </li>
+      </ul>
     </div>
   </nav>;
 };
+
+export default Navbar;
