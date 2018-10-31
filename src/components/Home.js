a1baf1eef3ab0d4895aa62bd9a15f3fa6d62f109
shan5742 <asamshan456@gmail.com>
2018-10-31 12:29:19 +0000

    creating components folder and adding home component

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        commodi enim corrupti nostrum similique consectetur natus explicabo
+        tenetur. Ea ipsa laboriosam quasi dolorum necessitatibus praesentium
+        accusamus eum expedita perferendis sit.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
