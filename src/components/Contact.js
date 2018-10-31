3b4728a96dbc0334f62b93c2db5134f510d83c0e
shan5742 <asamshan456@gmail.com>
2018-10-31 12:40:22 +0000

    adding contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        commodi enim corrupti nostrum similique consectetur natus explicabo
+        tenetur. Ea ipsa laboriosam quasi dolorum necessitatibus praesentium
+        accusamus eum expedita perferendis sit.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
