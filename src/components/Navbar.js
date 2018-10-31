c1bb6978d5f0f96baffef5b9e00c76bc84487880
shan5742 <asamshan456@gmail.com>
2018-10-31 13:23:11 +0000

    changing a tags to link tags utilizig react router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,25 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  <nav className="nav-wrapper red darken-3">
-    <div className="container">
-      <a className="brand-logo">Mario World</a>
-      <ul className="right">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/About">About</a>
-        </li>
-        <li>
-          <a href="/Contact">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </nav>;
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <a className="brand-logo">Mario World</a>
+        <ul className="right">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
