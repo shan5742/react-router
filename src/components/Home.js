40010ec7844831443954119ddb22a9d41d27e8cc
shan5742 <asamshan456@gmail.com>
2018-10-31 14:18:06 +0000

    commenting out prgrammatic redirect and setting up axios with json placeholder data

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,36 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Home extends Component {
-  componentDidMount;
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      this.setState({
+        psts: res.data.slice(0, 10)
+      });
+    });
+  }
   render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    );
     return (
       <div className="container">
         <h4 className="center">Home</h4>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          commodi enim corrupti nostrum similique consectetur natus explicabo
-          tenetur. Ea ipsa laboriosam quasi dolorum necessitatibus praesentium
-          accusamus eum expedita perferendis sit.
-        </p>
+        {postList}
       </div>
     );
   }
